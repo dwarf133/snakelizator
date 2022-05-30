352f22b8f8340b43f4a86843de4bf8dd1a9c0904
dwarf133 <you@example.com>
2022-05-31 02:55:27 +0500

    added falsk and adding jsons to redis

--- redis/db_connect.py
+++ redis/db_connect.py
@@ -1,5 +1,7 @@
 import json
 from pprint import pprint
+from symtable import SymbolTableFactory
+from unicodedata import name
 import yaml
 import redis
 
@@ -14,10 +16,17 @@ def insert_test_data():
     with open('../test/test.json', 'r') as file:
         vacancies = json.load(file)
         pprint(vacancies['items'][0])
-    # with r.pipeline() as pipe:
-    #     for temp in vacancies['items']:
-    #         pipe.mset([temp['id'], temp])
-    #     pipe.execute()
+    with r.pipeline() as pipe:
+        for temp in vacancies['items']:
+            print(temp['id'])
+            pipe.mset({temp['id']: json.dumps(temp)})
+        pipe.execute()
         
-insert_test_data()
-r.dbsize()
+# insert_test_data()
+print(r.dbsize())
+
+# поля для анализа: 
+#     name
+#     salary
+#     schedule
+#     snippet
